d89f4c19929484d966b437e8c1e01289d4b200f1
John <mail4tck@gmail.com>
2023-06-29 13:16:54 +0200

    working

--- yt_script.sh
+++ yt_script.sh
@@ -4,5 +4,5 @@ readarray -d \; -t LINKS_ARR <<< "$LINKS"
 for (( n=0; n < ${#LINKS_ARR[*]}; n++))
 do
   echo "Current link: ${LINKS_ARR[n]}"
-  python -m yt-dlp --download-archive "/videos/archive" --ignore-errors --no-overwrites --yes-playlist -f best/bestvideo+bestaudio -o "/videos/%(playlist)s/%(title)s-%(id)s.%(ext)s" "${LINKS_ARR[n]}"
+  yt-dlp --download-archive "/videos/archive" --ignore-errors --no-overwrites --yes-playlist -f best/bestvideo+bestaudio -o "/videos/%(playlist)s/%(title)s-%(id)s.%(ext)s" "${LINKS_ARR[n]}"
 done
